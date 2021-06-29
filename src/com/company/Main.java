8ac596c4e41b110a542d36f708ca65865fe9123c
Antoni Guss <39112558+AnthonyGu55@users.noreply.github.com>
2021-06-29 22:53:09 +0200

    Zadanie 11 - Garage

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,8 +4,6 @@ import com.company.creatures.Animal;
 import com.company.creatures.Pet;
 import com.company.devices.Car;
 import com.company.devices.Diesel;
-import com.company.devices.Electric;
-import com.company.devices.LPG;
 
 import java.util.Arrays;
 
@@ -15,28 +13,26 @@ public class Main {
 
         Animal dog = new Pet("dog");
 
-        Car car1 = new Diesel("heighten", "row", 902, 570.65);
-        Car car2 = new Diesel("none", "home", 169, 69.93);
-
-        Car car3 = new Electric("another", "because", 883, 673.42);
-        Car car4 = new LPG("band", "pause", 241, 917.79);
+        Car car0 = new Diesel("scion", "xd", 2011, 15000.00);
+        Car car1 = new Diesel("saturn", "sl1", 2001, 6000.00);
+        Car car2 = new Diesel("jeep", "grand cherokee", 2015, 40000.00);
+        Car car3 = new Diesel("polaris", "xplorer 400l", 2002, 5000.00);
 
         Human human1 = new Human(dog, 2000.0);
-        human1.setCash(10000.0);
+        human1.setCash(100000.0);
         Human human2 = new Human(dog, 3000.0);
-        human2.setCash(15000.0);
+        human2.setCash(150000.0);
 
-        human1.setCar(car1, 0);
-        human1.setCar(car2, 1);
+        human1.setCar(car0, 0);
+        human1.setCar(car1, 1);
 
-        human2.setCar(car3, 0);
-        human2.setCar(car4, 1);
+        human2.setCar(car2, 0);
+        human2.setCar(car3, 1);
 
         System.out.println(human1);
         System.out.println(human2);
 
-        human1.getCar(1).sell(human1, human2, 1.0);
-
+        car1.sell(human1, human2, 1.0);
 
         System.out.println(Arrays.toString(human1.getGarage()));
         System.out.println(Arrays.toString(human2.getGarage()));
